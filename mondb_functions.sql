752bffef9e8d006fc466258dca3abee781a5500d
Hannu Krosing <hannu@krosing.net>
2016-06-15 16:59:57 +0200

    cleaned up comments

--- mondb_functions.sql
+++ mondb_functions.sql
@@ -116,26 +116,7 @@ $$
 LANGUAGE plpgsql SECURITY DEFINER;
 
 /*
-CREATE OR REPLACE FUNCTION moninfo_2ndq.database_sizes_py() RETURNS SETOF moninfo_2ndq.mondata AS
-$$
-    for rec in plpy.execute("select datname as name, pg_database_size(datname) as value from pg_stat_database ORDER BY 1"):
-        yield rec
-$$
-LANGUAGE plpythonu SECURITY DEFINER;
-*/
-
-/*
-background writer
-  checkpoints_timed
-  checkpoints_req
-  checkpoint_write_time
-  checkpoint_sync_time
-  buffers_checkpoint
-  buffers_clean
-  maxwritten_clean
-  buffers_backend
-  buffers_backend_fsync
-  buffers_alloc
+values from pg_stat_bgwriter
 */
 
 CREATE OR REPLACE FUNCTION moninfo_2ndq.bgwriter(out name text, out value bigint) RETURNS SETOF RECORD AS
@@ -160,32 +141,7 @@ LANGUAGE plpgsql SECURITY DEFINER;
 
 
 /*
-transactions
-   transaction committed
-   transactions aborted
-hannu=# \d pg_stat_database
-          View "pg_catalog.pg_stat_database"
-     Column     |           Type           | Modifiers 
-----------------+--------------------------+-----------
- datid          | oid                      | 
- datname        | name                     | 
- numbackends    | integer                  | 
- xact_commit    | bigint                   | 
- xact_rollback  | bigint                   | 
- blks_read      | bigint                   | 
- blks_hit       | bigint                   | 
- tup_returned   | bigint                   | 
- tup_fetched    | bigint                   | 
- tup_inserted   | bigint                   | 
- tup_updated    | bigint                   | 
- tup_deleted    | bigint                   | 
- conflicts      | bigint                   | 
- temp_files     | bigint                   | 
- temp_bytes     | bigint                   | 
- deadlocks      | bigint                   | 
- blk_read_time  | double precision         | 
- blk_write_time | double precision         | 
- stats_reset    | timestamp with time zone | 
+server total info from pg_stat_database
 */
 
 CREATE OR REPLACE FUNCTION moninfo_2ndq.server_transactions(out name text, out value bigint) RETURNS SETOF RECORD AS
@@ -228,6 +184,10 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql SECURITY DEFINER;
+
+/*
+per-database total info from pg_stat_database
+*/
 
 CREATE OR REPLACE FUNCTION moninfo_2ndq.db_transactions(out name text, out value bigint) RETURNS SETOF RECORD AS
 $$
@@ -277,9 +237,10 @@ LANGUAGE plpgsql SECURITY DEFINER;
 
 
 /*
-longest transactions
+server longest transactions
    longest running transaction length
    longest running statement length
+   longest idle in transaction tupe
 */
 
 CREATE OR REPLACE FUNCTION moninfo_2ndq.longest_running_backend_states_ms(out name text, out value bigint) RETURNS SETOF RECORD AS
@@ -302,29 +263,10 @@ LANGUAGE plpgsql SECURITY DEFINER;
 
 
 /*
-tablespace sizes
+tablespace sizes and free space
   size and free for all tablespaces
   +min free space
 */
-
-/*
-CREATE OR REPLACE FUNCTION moninfo_2ndq.tablespace_sizes()
-RETURNS SETOF moninfo_2ndq.mondata AS
-$$
-    import os
-    data_directory = plpy.execute("select current_setting('data_directory')")[0]['current_setting']
-    tbspinf = plpy.execute('select spcname as name, pg_tablespace_size(oid) as size, pg_tablespace_location(oid) as location from pg_tablespace')
-    min_free_space = 0xffffffffffffffff
-    for row in tbspinf:
-        yield ('tablespace.%s.used' % row['name'], row['size']) 
-        locinfo = os.statvfs(row['location'] or data_directory)
-        free_space = locinfo.f_bfree * locinfo.f_bsize
-        yield ('tablespace.%s.free' % row['name'], free_space)
-        min_free_space = min(min_free_space, free_space)
-    yield ('tablespace._min_.free', min_free_space)
-$$ language plpythonu security definer;
-*/
-
 
 CREATE OR REPLACE FUNCTION moninfo_2ndq.tablespace_sizes()
 RETURNS SETOF moninfo_2ndq.mondata_int AS
@@ -347,54 +289,10 @@ pg_stat_functions total
   number of function calls
   average length of function call
   
-#track_functions = none                 # none, pl, all
-track_functions = all
+needs change in postgresql.conf 
+          
+track_functions = pl           # none, pl, all
   
-*/
-
-/*
-CREATE OR REPLACE FUNCTION moninfo_2ndq.functions_total(out name text, out value bigint) RETURNS SETOF RECORD AS
-$$
-DECLARE
-  total_calls bigint;
-  avg_time_ms float;
-  retval RECORD;
-BEGIN
-    SELECT sum(calls) AS total_calls,
-          (sum(total_time)/sum(calls)) AS avg_time
-      FROM pg_stat_user_functions
-      INTO total_calls, avg_time_ms; 
-    SELECT 'number_of_calls', total_calls INTO name, value;
-    RETURN NEXT;
-    SELECT 'avg_duration_mks', (avg_time_ms * 1000)::bigint INTO name, value;
-    RETURN NEXT;
-END;
-$$
-LANGUAGE plpgsql SECURITY DEFINER;
-*/
-
-/*
-CREATE OR REPLACE FUNCTION moninfo_2ndq.usr_function_totals_per_db()
-RETURNS SETOF moninfo_2ndq.mondata AS
-$$
-    import psycopg2
-    import psycopg2.extras
-    db_port = plpy.execute("select current_setting('port')")[0]['current_setting']
-    for dbrow in plpy.execute("select datname from pg_database where not datistemplate"):
-        database = dbrow['datname']
-#        con = psycopg2.connect('dbname=%s' % database)
-        con = psycopg2.connect('dbname=%s port=%s' % (database, db_port))
-        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("""
-        SELECT sum(calls)::bigint AS total_calls,
-              (sum(total_time)/sum(calls)*1000)::float::bigint AS avg_time
-          FROM pg_stat_user_functions
-        """)
-        for row in cur.fetchall():
-            yield ('database.%s.functions.user_totals.number_of_calls' % database, row['total_calls']) 
-            yield ('database.%s.functions.user_totals.avg_time_mks' % database, row['avg_time'])
-        con.close()
-$$ LANGUAGE plpythonu SECURITY DEFINER;
 */
 
 -- select * from moninfo_2ndq.usr_function_totals_per_db();
@@ -441,38 +339,8 @@ pg_stat_user_xxx for selected tables and indexes
 */
 
 /*
-CREATE OR REPLACE FUNCTION moninfo_2ndq.usr_object_totals_per_db_old()
-RETURNS SETOF moninfo_2ndq.mondata AS
-$$
-    import psycopg2, re
-    import psycopg2.extras
-    ftlookup = {26:'oid', 19:'name', 20:'bigint', 1184:'timestamptz'}
-    db_port = plpy.execute("select current_setting('port')")[0]['current_setting']
-    for dbrow in plpy.execute("select datname from pg_database where not datistemplate"):
-        database = dbrow['datname']
-#        plpy.notice("connecting to %s" % database)
-        con = psycopg2.connect('dbname=%s port=%s' % (database, db_port))
-#        plpy.notice('dbname=%s port=%s' % (database, db_port))
-        cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        for table_name in ['pg_stat_user_tables', 'pg_statio_user_tables', 'pg_stat_user_indexes', 'pg_statio_user_indexes']:
-            try:
-                cur.execute('select * from %s limit 1' % table_name)
-                ftypes =  [(fname, ftlookup[ftype]) for (fname, ftype) in [row[:2] for row in cur.description]]
-                stotals_query = 'SELECT %s FROM %s' % (
-                    ','.join(['sum(%(fname)s)::bigint as %(fname)s' % locals() for (fname,ftype) in ftypes if ftype == 'bigint']),
-                    table_name
-                    )
-                cur.execute(stotals_query)
-                for row in cur.fetchall():
-                    tname = re.sub(r'pg_stat(io)?_user_', '', table_name)
-                    row = dict(row)
-                    for (key, value) in row.items():
-                        yield 'database.%s.%s.user_totals.%s' % (database,tname,key),  value or 0
-            except:
-                yield ('error: %s' % sys.exc_info()), -1
-$$ LANGUAGE plpythonu SECURITY DEFINER;
+per-database totals for table and index integer parameters from pg_stat(io)_user_(indexes|tables)
 */
-
 
 CREATE OR REPLACE FUNCTION moninfo_2ndq.usr_object_totals_per_db()
 RETURNS SETOF moninfo_2ndq.mondata_int AS
@@ -539,70 +407,7 @@ $$ LANGUAGE plpythonu SECURITY DEFINER;
 
 
 
--- select count(*) from moninfo_2ndq.usr_object_totals_per_db();
 
-
-/*
-item counts (per database)
-   number of tables
-   number of indexes
-   number of functions
-   number of temp tables
-*/
-
-/*
-CREATE OR REPLACE FUNCTION moninfo_2ndq.object_counts()
-RETURNS SETOF moninfo_2ndq.mondata AS
-$$
-    import psycopg2
-    db_port = plpy.execute("select current_setting('port')")[0]['current_setting']
-    for row in plpy.execute("select datname from pg_database where not datistemplate"):
-        database = row['datname']
-#        con = psycopg2.connect('dbname=%s' % database)
-        con = psycopg2.connect('dbname=%s port=%s' % (database, db_port))
-        cur = con.cursor()
-        cur.execute('select count(*) from pg_stat_user_tables')
-        yield 'database.%s.tables.user_total.count' % database, cur.fetchone()[0]
-        cur.execute('select count(*) from pg_stat_user_indexes')
-        yield 'database.%s.indexes.user_total.count' % database, cur.fetchone()[0]
-        # cout user functions
-        cur.execute("""SELECT count(*)
-                         FROM pg_catalog.pg_proc p
-                         LEFT JOIN pg_catalog.pg_namespace n
-                           ON n.oid = p.pronamespace
-                        WHERE n.nspname not in ('information_schema', 'pg_catalog')""")
-        yield 'database.%s.functions.user_total.count' % database, cur.fetchone()[0]
-        con.close()
-$$ language plpythonu security definer;
-*/
-
-/*
-item sizes per user
-   size of tables
-   size of indexes
-   size of temp tables
-*/
-
-/*
-CREATE OR REPLACE FUNCTION moninfo_2ndq.object_sizes()
-RETURNS SETOF moninfo_2ndq.mondata AS
-$$
-    import psycopg2
-    db_port = plpy.execute("select current_setting('port')")[0]['current_setting']
-    for row in plpy.execute("select datname from pg_database where not datistemplate"):
-        database = row['datname']
-#        con = psycopg2.connect('dbname=%s' % database)
-        con = psycopg2.connect('dbname=%s port=%s' % (database, db_port))
-        cur = con.cursor()
-        cur.execute('select sum(pg_relation_size(relid)), sum(pg_total_relation_size(relid)) from pg_stat_user_tables')
-        rel_size, total_rel_size = cur.fetchone() 
-        yield 'database.%s.tables.total.rel_size' % database, rel_size
-        yield 'database.%s.tables.total.total_size' % database, total_rel_size
-        cur.execute('select sum(pg_relation_size(relid)) from pg_stat_user_indexes')
-        yield 'database.%s.indexes.total.rel_size' % database, cur.fetchone()[0]
-        con.close()
-$$ language plpythonu security definer;
-*/
 
 /*
 temp space usage:
